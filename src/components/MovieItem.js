00c0c64e6fd5fc1b37d28a4d1b7b74937498641b
郑旭 <forounder@163.com>
2019-04-26 15:26:05 +0800

    修改电影简介的样式

--- src/components/MovieItem.js
+++ src/components/MovieItem.js
@@ -1,16 +1,24 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import style from '../style/main.css'
 class MovieItem extends Component {
+    static propTypes = {
+        subject: PropTypes.object
+    }
     constructor(props) {
         super(props);
         this.state = {  }
     }
     render() {
-        console.log(this.props.subject)
+        let {title,id,images,rating} = this.props.subject;
+        console.log(style)
         return (
-            <div>
-                {this.props.subject.title}
-            </div>
+            <section>
+                <div className={style.image}>
+                    <img src={images.small} alt=""/>
+                </div>
+
+            </section>
          );
     }
 }
